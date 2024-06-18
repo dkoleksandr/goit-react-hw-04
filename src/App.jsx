4ea5851321783aedc15dfdbee26a051601126241
dkoleksandr <didenk.oleksandr@gmail.com>
2024-06-18 16:57:16 +0300

    add main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,71 @@
 import { useEffect, useState } from "react";
-import { nanoid } from "nanoid";
 import "./App.css";
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from "./components/ContactList/ContactList";
+import { getSearchApi } from "./api/search-api";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImageModal from "./components/ImageModal/ImageModal";
+import Loader from "./components/Loader/Loader";
 
 const App = () => {
-  const initialContacts = [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ];
-
-  const [searchValue, setSearchValue] = useState("");
-
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem("saved-contacts");
-
-    if (savedContacts !== null) {
-      return JSON.parse(savedContacts);
-    }
-    return initialContacts;
-  });
-
-  const handleChangeSearch = (e) => {
-    return setSearchValue(e.target.value);
-  };
-
-  const filteredContacts = contacts.filter((contact) => {
-    if (!searchValue) {
-      return contacts;
-    }
-    return contact.name
-      .toLocaleLowerCase()
-      .includes(searchValue.toLocaleLowerCase());
-  });
-
-  const handleSubmit = (values, actions) => {
-    setContacts((prevContacts) => [
-      ...prevContacts,
-      { ...values, id: nanoid() },
-    ]);
-
-    actions.resetForm();
-  };
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
 
   useEffect(() => {
-    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts));
-  }, [contacts]);
+    const fetchData = async () => {
+      try {
+        setError(false);
+        setLoading(true);
 
-  const handleDelete = (contactId) => {
-    setContacts((prevContacts) =>
-      prevContacts.filter((contact) => {
-        return contact.id !== contactId;
-      })
-    );
+        const data = await getSearchApi(query, page);
+        setPhotos((prev) => [...prev, ...data]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    query && fetchData();
+  }, [page, query]);
+
+  const handleSearch = async (searchQuery) => {
+    setQuery(searchQuery);
+  };
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  const openModal = (image) => {
+    setSelectedPhoto(image);
+  };
+
+  const closeModal = () => {
+    setSelectedPhoto(null);
   };
 
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm handleSubmit={handleSubmit} />
-      <SearchBox searchValue={searchValue} handleChange={handleChangeSearch} />
-      <ContactList contacts={filteredContacts} handleDelete={handleDelete} />
-    </div>
+    <>
+      <SearchBar onSubmit={handleSearch}></SearchBar>
+      {error && (
+        <p>
+          <b>Oops....some error! pls reload</b>
+        </p>
+      )}
+      {photos.length > 0 && (
+        <ImageGallery photos={photos} onClick={openModal} />
+      )}
+      {isLoading && <Loader />}
+      {photos.length > 0 && <button onClick={handleLoadMore}>Load more</button>}
+
+      <ImageModal
+        isOpen={Boolean(selectedPhoto)}
+        onRequestClose={closeModal}
+        photoForModal={selectedPhoto}
+      />
+    </>
   );
 };
 
---
+++ src/api/search-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+require("dotenv").config();
+
+axios.defaults.baseURL = "https://api.unsplash.com/";
+
+export const getSearchApi = async (searchQuery, page) => {
+  const { data } = await axios.get("/search/photos", {
+    params: {
+      query: searchQuery,
+      client_id: process.env.Access_Key,
+      per_page: 9,
+      page,
+    },
+  });
+  return data.results;
+};
