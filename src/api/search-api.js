c7084f45510486ac3845bec527bf1d344fb99eb5
dkoleksandr <didenk.oleksandr@gmail.com>
2024-06-18 17:05:48 +0300

    Update search-api.js

--- src/api/search-api.js
+++ src/api/search-api.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-import 'dotenv/config'
-// require("dotenv").config();
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
@@ -8,7 +6,7 @@ export const getSearchApi = async (searchQuery, page) => {
   const { data } = await axios.get("/search/photos", {
     params: {
       query: searchQuery,
-      client_id: process.env.Access_Key,
+      client_id: '6FT8qaNMbVdsDnvvjia-ve_gP6sr4q8g672jAkrxEfk',
       per_page: 9,
       page,
     },
